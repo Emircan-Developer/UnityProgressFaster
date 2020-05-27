93b44df2267970dbb3c65b6c2dd8daf48f756a19
Co-Developer <61265158+Emircan-Developer@users.noreply.github.com>
2020-05-27 18:01:34 +0300

    BowFire

---
+++ Assets/Firing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject Arrow;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+           float degree =  Mathf.Rad2Deg * Mathf.Atan2(Diffrence().y,Diffrence().x);
+            GameObject arrow =Instantiate(Arrow,transform.position,Quaternion.AngleAxis(degree,Vector3.forward));
+            Rigidbody2D rb = arrow.AddComponent<Rigidbody2D>();
+            rb.AddForce(arrow.transform.right * 250f);
+            rb.gravityScale = 0;
+            Debug.Log(degree);
+        }
+    }
+    Vector3 MousePos()
+    {
+        return cam.ScreenToWorldPoint(Input.mousePosition) ;
+    }
+    Vector3 Diffrence()
+    {
+        return (MousePos() - transform.position);
+    }
+}
